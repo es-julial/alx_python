645d0649c039e5255a4081efd3419e6ba42eae53
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-07-17 01:13:51 +0200

    5-6_updated

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(100):
-    if i != 99:
-        print("{:02d},".format(i), end=" ")
-    else:
-        print("{:02d}".format(i), end="\n")
+    print("{:02d}".format(i), end=', ' if i < 99 else '\n')
--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    for x in range(i+1, 10):
-        if i == 8 and x == 9:
-            print("{:d}{:d}".format(i, x), end="\n")
-        else:    
-            print("{:d}{:d},".format(i, x), end=" ")
+for i in range(10):
+    for j in range(i+1, 10):
+        print("{:d}{:d}".format(i, j), end=', ' if i < 8 else '\n')
