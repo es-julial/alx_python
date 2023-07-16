1bab36b5d8056cbcc32f3b7a10a790d26dad7bb4
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-07-17 00:43:49 +0200

    1-edges.py

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,7 @@
+word = "Holberton"
+middle_word = word[1:-1]
+word_last_2 = word[-2:]
+word_first_3 = word[:3]
+print("First 3 letters: {}".format(word_first_3))
+print("Last 2 letters: {}".format(word_last_2))
+print("Middle word: {}".format(middle_word))
