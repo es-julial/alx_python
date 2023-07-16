86527a0df99678a3368e2d8e6882dd698fa2403d
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-07-17 01:33:20 +0200

    5-print_comb2.py

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i != 99:
-        print("{:02d},".format(i), end=" ")
-    else:
-        print("{:02d}".format(i), end="\n")
+for num in range(100):
+    print("{:02d}".format(num), end=", " if num != 99 else "\n")
