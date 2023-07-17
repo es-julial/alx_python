22338759868f01f0c5f41da7963dae71dcf18d0a
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-07-17 02:25:42 +0200

    Python - Functions

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,5 +1,3 @@
 def reverse_string(string):
-    reversed_str = ""
-    for char in string:
-        reversed_str = char + reversed_str
-    return reversed_str
+    reversed = string[::-1]
+    return (reversed)
