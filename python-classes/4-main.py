74042d63f630adb50e784bf4347a239af6114b53
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-01 00:00:17 +0200

    T4

--- python-classes/4-main.py
+++ python-classes/4-main.py
@@ -1,4 +1,4 @@
-# !/usr/bin/python3
+#!/usr/bin/python3
 Square = __import__('4-square').Square
 
 my_square = Square(3)
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """This module defines a Square class.
 
 The Square class represents a square with a private instance attribute 'size'.
