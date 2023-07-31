813a6165d98760fc9a8714b61b3f4f420e877470
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-01 00:35:18 +0200

    T4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -27,7 +27,7 @@ class Square:
         __size (int): The size of the square.
 
     Methods:
-        _init_(self, size=0): Initializes a square object with an optional size.
+        _init_(self, size=0): Initializes square obj with optional size.
         size(self): Getter method to retrieve the size of the square.
         size(self, value): Setter method to set the size of the square.
         area(self): Calculates and returns the area of the square.
