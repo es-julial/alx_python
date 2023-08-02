f5d968bbbe8d603e442a2021ec0b7959dd805f3a
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-02 17:23:33 +0200

    Inheritance

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,38 +1,19 @@
-def is_same_class(obj, a_class):
-    """
-    Check if the object is exactly an instance of the specified class.
+"""
+This module checks if an object is the instance of a class
 
-    Parameters:
-        obj: Any - The object to check.
-        a_class: type - The class to compare against.
+"""
+def is_same_class(obj, a_class):
+    """Check if object is axact instance of a class
+
+    Args:
+        obj: An object instance
+        a_class: a class
 
     Returns:
-        bool: True if the object is an instance of the specified class; otherwise, False.
+        True: If object is instance of the class
+        False: if object not imstance of the class
     """
-    return type(obj) == a_class
-
-
-# Test cases
-a = 1
-print(is_same_class(a, int))  # Correct output: True
-
-a = True
-print(is_same_class(a, int))  # Correct output: False
-
-a = 3.14
-print(is_same_class(a, int))  # Correct output: False
-
-a = True
-print(is_same_class(a, object))  # Correct output: False
-
-a = None
-print(is_same_class(a, object))  # Correct output: True
-
-a = None
-print(is_same_class(a, list))  # Correct output: False
-
-a = [1, 2, 3]
-print(is_same_class(a, list))  # Correct output: True
-
-a = [1, 2, 3]
-print(is_same_class(a, object))  # Correct output: False
+    if type(obj) == a_class:
+       return True
+    else:
+        return False
