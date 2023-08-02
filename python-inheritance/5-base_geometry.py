1127335ff9ccb55010eaed712b3629fe7cac27ce
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-02 17:43:49 +0200

    T5

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Create BaseGeometry"""
+"""Create class BaseGeometry"""
 
 
 class NoInitSubclassMeta(type):
@@ -9,7 +9,7 @@ class NoInitSubclassMeta(type):
 
 
 class BaseGeometry(metaclass=NoInitSubclassMeta):
-    """BaseGeometry class
+    """Empty class
     """
     def __dir__(cls):
         """Removing __init_subclass_ attribute
@@ -19,9 +19,26 @@ class BaseGeometry(metaclass=NoInitSubclassMeta):
                 attr != '__init_subclass__']
 
     def area(self):
-        """Define Area function.
+        """Area function.
 
         Raises:
             Exception: if area is not implemented.
         """
         raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates.
+
+        Args:
+            name (str): name of the object.
+            value (int): value of the property.
+
+        Raises:
+            TypeError: if value is not an integer.
+            ValueError: if value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
