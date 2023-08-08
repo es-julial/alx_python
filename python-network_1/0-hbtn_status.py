231d95fafa8a4004e7250078861481679ef423db
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 14:22:14 +0200

    T0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
-Python script that fetches an URL with requests package
+Module to fetch and display the status of a website.
 """
+
 import requests
 
+def fetch_hbtn_status():
+    """
+    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
+    """
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content_type = type(response.text)
+    content = response.text
+
+    print("Body response:")
+    print(f"\t- type: {content_type}")
+    print(f"\t- content: {content}")
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))x
+    fetch_hbtn_status()
