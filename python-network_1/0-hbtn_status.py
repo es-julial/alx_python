efcf53efed41997cae46f51d051267463faf0dcd
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 13:51:50 +0200

    T0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,4 +9,3 @@ if __name__ == "__main__":
     print("Body response:")
     print('\t- type: {}'.format(type(html)))
     print('\t- content: {}'.format(html))
-    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
