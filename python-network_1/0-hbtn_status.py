5793dd22d859b15f4c701cb16ec124ac563de566
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 14:51:42 +0200

    T0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -13,10 +13,10 @@ def fetch_hbtn_status():
     response = requests.get(url)
     content_type = type(response.text)
     content = response.text
-
+   
     print("Body response:")
     print(f"\t- type: {content_type}")
     print(f"\t- content: {content}")
-
+   
 if __name__ == "__main__":
     fetch_hbtn_status()
