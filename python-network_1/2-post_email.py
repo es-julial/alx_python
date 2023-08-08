4b2c0c028fa76f7b3688711c50aa1db433bf215e
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 15:10:09 +0200

    updated

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 """Sends a POST request to a given URL with a given email.
-Usage: ./6-post_email.py <URL> <email>
+Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
 import sys
-import requests
+import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value).encode("ascii")
 
-    r = requests.post(url, data=value)
-    print(r.text)
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
