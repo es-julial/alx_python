4731a3130daacad3b1f92ae36cfe0a6eb0ab338b
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 13:54:08 +0200

    T0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,22 @@
-#!/usr/bin/python3
-"""script that fetches 'https://alu-intranet.hbtn.io/status' """
-if __name__ == "__main__":
-    import urllib.request
+#!/usr/bin/env python3
+"""
+Module to fetch and display the status of a website.
+"""
 
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') \
-            as response:
-        html = response.read()
+import requests
+
+def fetch_hbtn_status():
+    """
+    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
+    """
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content_type = type(response.text)
+    content = response.text
+
     print("Body response:")
-    print('\t- type: {}'.format(type(html)))
-    print('\t- content: {}'.format(html))
+    print(f"\t- type: {content_type}")
+    print(f"\t- content: {content}")
+
+if __name__ == "__main__":
+    fetch_hbtn_status()
