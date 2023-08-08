9b32df425501e34d820e0d29388089d709ae936c
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 14:57:00 +0200

    T1

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the value of the
-   X-Request-Id variable found in the header of the response"""
-import urllib.request as request
-from sys import argv
+"""Fetches the value of the X-Request-Id header from a given URL's response"""
 
+import requests
+import sys
+
+def main():
+    """Main function to fetch and display the X-Request-Id header"""
+    if len(sys.argv) != 2:
+        print("Usage: ./1-hbtn_header.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    response = requests.get(url)
+    x_request_id = response.headers.get('X-Request-Id')
+
+    if x_request_id is not None:
+        print(x_request_id)
 
 if __name__ == "__main__":
-    req = request.Request(argv[1])
-    with request.urlopen(req) as r:
-        print(r.headers.get('X-Request-Id'))
+    main()
