3d1d6c321d28bc90478a722741bb685512e6c9e5
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-08 13:47:49 +0200

    T0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""script that fetches 'https://alx-intranet.hbtn.io/status' """
+"""script that fetches 'https://alu-intranet.hbtn.io/status' """
 if __name__ == "__main__":
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') \
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') \
             as response:
         html = response.read()
     print("Body response:")
