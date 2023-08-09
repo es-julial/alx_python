2be03726da7981d4fa0b7d386ca155c4e5950893
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-09 12:47:27 +0200

    Python-Network_updated

--- python-network_1/3-post_email.py
+++ python-network_1/3-post_email.py
@@ -1,15 +1,39 @@
 #!/usr/bin/python3
-"""takes url & email, sends a POST request and displays the response"""
+"""
+This is a Python script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response.
+"""
 import requests
 import sys
 
 def send_post_request(url, email):
-    data = {'email': email}
-    response = requests.post(url, data=data)
-    return response.text
+    """
+    Sends a POST request to the provided URL with the email as a parameter and displays the response body.
+
+    Args:
+        url (str): The URL to send the POST request to.
+        email (str): The email address to include as a parameter in the request.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs during the HTTP request.
+    """
+    payload = {"email": email}
+
+    response = requests.post(url, data=payload)
+
+    response.raise_for_status()
+
+    print("Response body:")
+    print(response.text)
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    email = sys.argv[2]
-    response_body = send_post_request(url, email)
-    print(response_body)
+    if len(sys.argv) < 3:
+        print("Please provide a URL and an email address as command-line arguments.")
+    else:
+        url = sys.argv[1]
+        email = sys.argv[2]
+        try:
+            send_post_request(url, email)
+        except requests.exceptions.RequestException as e:
+            print("Error:", str(e))
