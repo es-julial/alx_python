0c3d9f092f0e464a848e95a6946cc7bf8d42dba7
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-10 22:25:13 +0200

    Python-Network_updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,12 @@
-#!/usr/bin/env python3
-"""
-Module to fetch and display the status of a website.
-"""
-
-import requests
-
-def fetch_hbtn_status():
-    """
-    Fetches and displays the status of https://alu-intranet.hbtn.io/status.
-    """
-    url = 'https://alu-intranet.hbtn.io/status'
-    response = requests.get(url)
-    content_type = type(response.text)
-    content = response.text
-   
-    print("Body response:")
-    print(f"\t- type: {content_type}")
-    print(f"\t- content: {content}")
-   
+#!/usr/bin/python3
+"""script that fetches 'https://alu-intranet.hbtn.io/status' """
 if __name__ == "__main__":
-    fetch_hbtn_status()
+    import urllib.request
+
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') \
+            as response:
+        html = response.read()
+    print("Body response:")
+    print('\t- type: {}'.format(type(html)))
+    print('\t- content: {}'.format(html))
+    print('\t- utf8 content: {}'.format(html.decode('UTF8')))
