8658c7ebb33c1162e849a57334dc3f839100d605
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-10 20:35:51 +0200

    Python-Network_updated

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
-"""Sends a request to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
-"""
-import sys
-import requests
+"""Sends a request to a URL and displays the response body, handling error codes"""
 
+import requests
+import sys
+
+def main():
+    """Main function to send request and handle response"""
+    if len(sys.argv) != 2:
+        print("Usage: ./4-error_code.py <URL>")
+        sys.exit(1)
+
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print("Error code:", response.status_code)
+    else:
+        print(response.text)
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    r = requests.get(url)
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
-    else:
-        print(r.text)
+    main()
