6ff8025c41e928b213dd0eae9f5921a0a154ade6
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-10 20:58:49 +0200

    Python-Network_updated

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,15 +1,31 @@
+#!/usr/bin/python3
 """
-    Fetches a URL, sends a request to the URL, and displays the body of the
-    response. Prints an error message if the HTTP status code is >= 400.
+This is a Python script that takes in a URL,
+sends a request to the URL and displays the body of the response.
 """
-
-
-
 import requests
 import sys
 
-if __name__ == "__main__":
-    res = requests.get(sys.argv[1])
-    print(res.text)
-    if res.status_code >= 400:
-        print(f"Error code: {res.status_code}")
+def fetch_response_body(url):
+    """
+    Fetches the body of the response from the given URL and prints it.
+
+    Args:
+        url (str): The URL to send the request to.
+
+    Raises:
+        requests.exceptions.RequestException: If an error occurs during the HTTP request.
+    """
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    try:
+        fetch_response_body(url)
+    except requests.exceptions.RequestException as e:
+        print("Error:", str(e))
