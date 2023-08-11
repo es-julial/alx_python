9aeb72a4a503a7a4bbb4f2ef84d9692acd8bf5fa
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-11 03:07:55 +0200

    Python-Network_updated

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,44 @@
 #!/usr/bin/python3
+
+"""
+This script fetches the status information from a URL using the 'requests' package
+and displays the body of the response with tabulation.
+"""
+
 import requests
 
-url = "https://alu-intranet.hbtn.io/status"
+class StatusFetcher:
+    """
+    A class to fetch and display status information from a URL.
+    """
 
-response = requests.get(url)
+    def __init__(self, url):
+        """
+        Initialize the StatusFetcher with the given URL.
 
-if response.status_code == 200:
-    content = response.text
-    print("Body response:")
-    print("\t- type:", type(content))
-    print("\t- content:", content)
-else:
-    print("Error:", response.status_code)
+        Args:
+            url (str): The URL to fetch the status from.
+        """
+        self.url = url
+
+    def fetch_and_display_status(self):
+        """
+        Fetches the status information from the URL and displays the response body.
+
+        Returns:
+            None
+        """
+        response = requests.get(self.url)
+
+        if response.status_code == 200:
+            data = response.json()
+            print("Body response:")
+            print("\t- type:", type(data))
+            print("\t- content:", data)
+        else:
+            print("Error:", response.status_code)
+
+if __name__ == "__main__":
+    url = "https://alu-intranet.hbtn.io/status"
+    status_fetcher = StatusFetcher(url)
+    status_fetcher.fetch_and_display_status()
