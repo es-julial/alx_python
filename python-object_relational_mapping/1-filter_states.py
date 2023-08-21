31c05409de7937fece9d7756b5a9eeee6f277d0a
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-21 08:39:57 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -21,7 +21,7 @@
 # CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
 # USE hbtn_0e_0_usa;
 # CREATE TABLE IF NOT EXISTS states (
-# id INT NOT NULL AUTO_INCREMENT, 
+# id INT NOT NULL AUTO_INCREMENT,
 # name VARCHAR(256) NOT NULL,
 #  PRIMARY KEY (id)
 # );
@@ -56,4 +56,3 @@ if __name__ == '__main__':
     rows = cur.fetchall()
     for row in rows:
         print(row)
-        
