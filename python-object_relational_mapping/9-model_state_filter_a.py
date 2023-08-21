143ba835fe90499a2baade958076907b1db2ea27
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-21 09:16:10 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
-"""lists all State objects that contain the
-letter a from the database hbtn_0e_6_usa"""
+# 9-model_state_filter_a.py
+"""import a model and fitch the states where it has letetr a"""
 
 from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
-    InstanceSession = sessionmaker(bind=engine)
-    session = InstanceSession()
-    states = session.query(State).filter(
-        State.name.contains('a')).order_by(State.id).all()
-    for state in states:
-        print('{}: {}'.format(state.id, state.name))
+        'mysql+mysqldb://{}:{}@localhost/{}'.format
+        (argv[1],
+         argv[2],
+         argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).filter(State.name.like('%a%'))
+
+    for i in states:
+        print('{}: {}'.format(i.id, i.name))
+
     session.close()
