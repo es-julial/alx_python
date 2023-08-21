6b402d5342845fde9c750b6da29f3181fc229356
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-21 09:01:08 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,28 +1,3 @@
-
-# 9. Contains `a`
-# mandatory
-
-# Write a script that lists all State objects that contain the letter a from the database hbtn_0e_6_usa
-
-#     Your script should take 3 arguments: mysql username, mysql password and database name
-#     You must use the module SQLAlchemy
-#     You must import State and Base from model_state - from model_state import Base, State
-#     Your script should connect to a MySQL server running on localhost at port 3306
-#     Results must be sorted in ascending order by states.id
-#     The results must be displayed as they are in the example below
-#     Your code should not be executed when imported
-
-# guillaume@ubuntu:~/$ ./9-model_state_filter_a.py root root hbtn_0e_6_usa
-# 1: California
-# 2: Arizona
-# 3: Texas
-# 5: Nevada
-# guillaume@ubuntu:~/$ 
-
-# No test cases needed
-
-# ///////////////////////////////////////////////
-
 #!/usr/bin/python3
 """lists all State objects that contain the
 letter a from the database hbtn_0e_6_usa"""
