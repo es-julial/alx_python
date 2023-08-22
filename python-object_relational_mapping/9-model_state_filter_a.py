58906d0704b82f6338c9181b8f7158521cdee1b1
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-22 06:52:42 +0200

    python-object_relational_mapping

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-# 9-model_state_filter_a.py
-"""import a model and fitch the states where it has letetr a"""
-
-from sys import argv
+""" prints the first State object from the database hbtn_0e_6_usa
+"""
+import sys
 from model_state import Base, State
-from sqlalchemy import create_engine
+from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
+
 if __name__ == "__main__":
-
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format
-        (argv[1],
-         argv[2],
-         argv[3]),
-        pool_pre_ping=True)
-
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    states = session.query(State).filter(State.name.like('%a%'))
-
-    for i in states:
-        print('{}: {}'.format(i.id, i.name))
-
-    session.close()
+    for instance in session.query(State).filter(State.name.like('%a%')):
+        print(instance.id, instance.name, sep=": ")
