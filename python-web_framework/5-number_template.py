d5fdc7da6fdabc9bb44d196662cc22eaeb65ebce
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-30 12:11:41 +0200

    PWF

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -1,37 +1,73 @@
-#!/usr/bin/python3
-'''
-Starts a Flask web application.
-'''
+"""
+A script that starts a Flask Web application and also has
+an additional route/path.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+"""
+using @app.route to map "/" to the url
+"""
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    return 'Hello HBNB!'
+        """
+        Route handler for the root URL '/'. Displays "Hello HBNB!".
+        """
+        return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return 'HBNB'
-
+    """
+    Route handler for '/hbnb'. Displays "HBNB".
+    """
+    return "HBNB"
+        
 @app.route('/c/<text>', strict_slashes=False)
-def display_c_text(text):
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+def custom_text(text):
+    """
+        Route handler for '/c/<text>'. Displays "C " followed by the value of the text variable.
+        Replace underscore _ symbols with spaces.
+
+        Args:
+            text (str): The text variable captured from the URL.
+        """
+    modified_text = text.replace('_', ' ')
+    return "C {}".format(modified_text)
 
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def display_python_text(text):
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
+def python_text(text):
+    """
+            Route handler for '/python/<text>'. Displays "Python " followed by the value of the text variable.
+            Replace underscore _ symbols with spaces.
+
+            Args:
+                text (str): The text variable captured from the URL.
+    """
+    modified_text = text.replace('_', ' ') if text else 'is cool'
+    return "Python {}".format(modified_text)
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
-    return '{} is a number'.format(n)
+def number(n):
+    """
+            Route handler for '/number/<n>'. Displays "<n> is a number" if n is an integer.
 
-@app.route('/5-number_template/<int:number>', strict_slashes=False)
-def display_number_template(number):
-    return render_template('5-number.html', number=number)
+            Args:
+                n (int): The number captured from the URL.
+    """
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+            """
+            Route handler for '/number_template/<n>'. Displays an HTML page with "Number: n" in an H1 tag.
+
+            Args:
+                n (int): The number captured from the URL.
+            """
+            return render_template('number_template.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
